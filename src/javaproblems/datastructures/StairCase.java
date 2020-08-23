9219ba8b00b6816a8c48b78f7155e99adfba0efd
MostafaYahia <mostafayehyax23@gmail.com>
2020-08-23 11:01:06 +0200

    Solve MinusPlus Solve StairCase

---
+++ src/javaproblems/datastructures/StairCase.java
@@ -0,0 +1,41 @@
+package javaproblems.datastructures;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class StairCase {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = n; j-i >0; j--)
+                System.out.print(" ");
+
+            for (int k = 1; k <= i; k++)
+                System.out.print("#");
+
+            System.out.println();
+        }
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
+
